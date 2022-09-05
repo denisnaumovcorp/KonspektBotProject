9a806c35a1699f9b1954206f763bc16e0fd580c5
Psycho <73433657+SShadowKeeper@users.noreply.github.com>
2022-09-05 19:38:09 +0300

    Conversation Handler

--- KonspektBot.py
+++ KonspektBot.py
@@ -4,6 +4,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 import telegram.ext.filters as filters
 from functools import partial
 import yadisk
+import copy
 
 disk = yadisk.YaDisk(token="y0_AgAAAABALxVvAAhfcwAAAADNm_GdxJ2gzsdISuSJfntSvJ0Kaydd99w")
 print(disk.check_token())
@@ -15,10 +16,10 @@ choice_reg = "(^Конспекты$|^Дз$)"
 days_reg = "(^\d{1}$|^\d{2}$)"
 action_reg = "(^Загрузить конспекты$|^Посмотреть конспекты$)"
 
-things_reply_keyboard = [["Конспекты", "Дз"]]
-years_reply_keyboard = [["2022", "2023", "2024"]]
-months_reply_keyboard = [["Сентябрь", "Октябрь", "Ноябрь"], ["Декабрь", "Январь", "Февраль"], ["Март", "Апрель", "Май"], ["Июнь", "Июль", "Август"]]
-subjects_reply_keyboard = [["Алгебра", "Геометрия", "Мат. анализ"], ["Русский язык", "Литература", "Английский язык"], ["Биология", "ОБЖ", "История"], ["Физика", "Химия", "Физ. лаба"], ["Обществознание"]]
+things_reply_keyboard = [["Конспекты"], ["Выйти"]]
+years_reply_keyboard = [["2022", "2023", "2024"], ["Выйти"]]
+months_reply_keyboard = [["Сентябрь", "Октябрь", "Ноябрь"], ["Декабрь", "Январь", "Февраль"], ["Март", "Апрель", "Май"], ["Июнь", "Июль", "Август"], ["Выйти"]]
+subjects_reply_keyboard = [["Алгебра", "Геометрия", "Мат. анализ"], ["Русский язык", "Литература", "Английский язык"], ["Биология", "ОБЖ", "История"], ["Физика", "Химия", "Физ. лаба"], ["Обществознание"], ["Выйти"]]
 
 ACTION, SUBJECT, YEAR, MONTH, DAY, UPLOAD_PHOTOS, GET_PHOTOS, SEE_PHOTOS = range(8)
 
@@ -41,18 +42,21 @@ def main() -> None:
                                                         MONTH: [MessageHandler(filters.Regex(years_reg), choose_a_month)],
                                                         DAY: [MessageHandler(filters.Regex(months_reg), choose_a_day)],
                                                         GET_PHOTOS: [MessageHandler(filters.Regex(days_reg), partial(get_or_see_photos, bot = bot))],
-                                                        UPLOAD_PHOTOS: [MessageHandler(filters.PHOTO | filters.Document.Category('image/'), partial(upload_photos, bot=bot))]}, fallbacks=[MessageHandler(filters.Regex("^Готов$"), start)])
+                                                        UPLOAD_PHOTOS: [MessageHandler(filters.PHOTO | filters.Document.Category('image/'), partial(upload_photos, bot=bot))]}, fallbacks=[MessageHandler(filters.Regex("^Выйти"), start)])
     application.add_handler(conv_handler)
 
 
     application.run_polling()
 
 async def start(update, context) -> int:
+    tempiter = copy.copy(context.user_data)
+    for i in tempiter:
+        del context.user_data[str(i)]
     await update.message.reply_text("Приветствую! Выберите, пожалуйста, то, что Вам нужно", reply_markup=ReplyKeyboardMarkup(keyboard=things_reply_keyboard, one_time_keyboard=False, resize_keyboard=True))
     return ACTION
 async def choose_an_action(update, context) -> int:
     context.user_data["choice"] = update.message.text
-    await update.message.reply_text("Что вы хотите сделать?", reply_markup=ReplyKeyboardMarkup(keyboard=[["Загрузить конспекты", "Посмотреть конспекты"]], one_time_keyboard=False, resize_keyboard=True))
+    await update.message.reply_text("Что вы хотите сделать?", reply_markup=ReplyKeyboardMarkup(keyboard=[["Загрузить конспекты", "Посмотреть конспекты"], ["Выйти"]], one_time_keyboard=False, resize_keyboard=True))
     return SUBJECT
 
 async def choose_a_subject(update, context) -> int:
@@ -83,12 +87,13 @@ async def choose_a_day(update, context) -> int:
         elif i == amount_of_days:
             reply_keyboard.append(temp)
             temp = []
+    reply_keyboard.append(["Выйти"])
     await update.message.reply_text("Выберите день: ", reply_markup=ReplyKeyboardMarkup(keyboard=reply_keyboard, one_time_keyboard=False, resize_keyboard=True))
     return GET_PHOTOS
 async def get_or_see_photos(update, context, bot) -> int:
     context.user_data["day"] = update.message.text
     if context.user_data["action"] == "Загрузить конспекты":
-        await update.message.reply_text("Отправте в чат изображения, которые вы хотите добавить")
+        await update.message.reply_text("Отправте в чат изображения, которые вы хотите добавить", reply_markup=ReplyKeyboardMarkup(keyboard=[["Выйти"]], one_time_keyboard=False, resize_keyboard=True))
         return UPLOAD_PHOTOS
     if context.user_data["action"] == "Посмотреть конспекты":
         path = 'conspectbot/' + context.user_data["subject"] + '/' + context.user_data["year"] + '/' + context.user_data["month"] + '/' + context.user_data["day"]
